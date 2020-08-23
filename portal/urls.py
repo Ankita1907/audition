60f78061bb21aa4a60a21e5f3894ab76e9e97e2f
Ankita1907 <ankitamandal19072000@gmail.com>
2020-08-23 19:45:16 +0530

    final

--- portal/urls.py
+++ portal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
      path('login/', views.signup, name='login'),
       path('rules/', views.rules, name='rules'),
        path('logout/', views.logout, name='logout'),
